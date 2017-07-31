f414dacd26227f8f2c03758d96004abe68e096e3
yomzed <martinezyoan@gmail.com>
2017-07-31 15:04:41 +0200

    Ajout name dans commentaires

--- database/migrations/2017_07_31_094530_create_comments_table.php
+++ database/migrations/2017_07_31_094530_create_comments_table.php
@@ -16,6 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('post_id');
+            $table->string('name')->default('anonymous');
             $table->text('content');
             $table->boolean('published')->default(false);
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('CASCADE');
