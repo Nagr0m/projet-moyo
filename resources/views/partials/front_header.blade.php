c1c97226e7e61c26ac9cc992fe334640d59bbbfd
Morgan de Lavenne <mo.delavenne@gmail.com>
2017-08-04 09:16:54 +0200

    Front diver

--- resources/views/partials/front_header.blade.php
+++ resources/views/partials/front_header.blade.php
@@ -1,12 +1,15 @@
-<header>
-	<div>
-		
-	</div>
+<div class="top">
+	<h1 class="title">Lycée Moyo</h1>
+	<div class="social"></div>
+</div>
 
-	<nav>
-		<a href="{{ route('home') }}">Home</a>
-		<a href="{{ route('actus') }}">Actualités</a>
-		<a href="{{ route('lycee') }}">Le lycée</a>
-	</nav>
+<div class="cover">
+	
+</div>
 
-</header>
+<nav class="bottom">
+	<a href="{{ route('home') }}">Home</a>
+	<a href="{{ route('actus') }}">Actualités</a>
+	<a href="{{ route('lycee') }}">Le lycée</a>
+	<input type="text">
+</nav>
