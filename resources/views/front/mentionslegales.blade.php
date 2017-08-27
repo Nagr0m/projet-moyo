b142f22a79705ae3bb1f80c0be3880b800857b36
yomzed <martinezyoan@gmail.com>
2017-08-27 15:09:48 +0200

    Mentions légales

---
+++ resources/views/front/mentionslegales.blade.php
@@ -0,0 +1,51 @@
+@extends ('layouts.front')
+@section ('title', 'Mentions légales')
+
+@section ('content')
+    <div class="page-head">
+        <div class="container">
+            <h2>Mentions légales</h2>
+        </div>
+    </div>
+
+    <div class="container">
+        <h3>Contact Publication du site</h3>
+        <p>Directeur de publication : M. Morgan et M. Yoan</p>
+
+        <h3>Hébergement du site</h3>
+        <p>
+            OVH
+            SAS au capital de 10 059 500 €
+            RCS Lille Métropole 424 761 419 00045
+            Code APE 6202A
+            N° TVA : FR 22 424 761 419
+            Siège social : 2 rue Kellermann – 59100 Roubaix – France.
+        </p>
+
+        <h3>Protection des données personnelles</h3>
+        <p>
+            Le lycée Moyo s'engage à préserver la confidentialité des informations éventuellement fournies en ligne par l'internaute. Toute information personnelle que l'internaute serait amené à transmettre à Moyo pour l'utilisation de certains services est soumise aux dispositions de la Loi n° 78-17 Informatique et Libertés du 06 janvier 1978. Moyo ne fait pas l'objet d'une déclaration à la CNIL
+        </p>
+        <p>
+            À ce titre, l'internaute dispose d'un droit d'accès, de rectification et de suppression des informations personnelles le concernant qu'il peut exercer à tout moment en adressant un courrier à notre adresse
+        </p>
+
+        <h3>Crédits Photographies</h3>
+
+        <p>Sergey Brin</p>
+
+        <h3>Droit de propriété</h3>
+        <p>
+            Le site ainsi que tout logiciel utilisé nécessairement en relation avec celui-ci peuvent contenir des informations confidentielles et protégées par le droit de la propriété intellectuelle en vigueur ou toute autre loi. Ainsi, sauf mention contraire, les droits de propriété intellectuelle sur les documents contenus dans le site et chacun des éléments créés pour ce site sont la propriété exclusive de Moyo, celle-ci ne concédant aucune licence, ni aucun droit autre que celui de consulter le site. La reproduction de tous documents publiés sur le site est seulement autorisée aux fins exclusives d'information pour un usage personnel et privé, toute reproduction et toute utilisation de copies réalisées à d'autres fins étant expressément interdite. Il est également interdit de copier, modifier, créer une oeuvre dérivée, inverser la conception ou l'assemblage ou de toute autre manière tenter de trouver le code source (à l'exception des cas prévus par la loi), vendre, attribuer, sous-licencier ou transférer de quelque manière que ce soit tout droit afférant aux logiciels. De même, il est également interdit de modifier le logiciel ou d'utiliser des versions modifiées des logiciels et notamment (sans que cette énumération soit limitative) en vue d'obtenir un accès non autorisé au service et d'accéder au site par un autre moyen que l'interface qui vous est fournie par le lycée Moyo à cet effet.
+        </p>
+        <p>
+            Le lycée se réserve le droit de demander des dommages et intérêts très chers en cas de contrefaçon et plus généralement d'atteinte à ses droits de propriété intellectuelle.
+        </p>
+
+        <p class="lead">
+            Bien entendu, tous ces textes et images sont factices.
+        </p>
+
+    </div>
+
+@endsection
